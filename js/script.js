ce2323ba2920d8fc59dd8afc5fa1976c9f5f010e
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-16 16:19:35 +0200

    implemented program to print result in page

--- js/script.js
+++ js/script.js
@@ -2,16 +2,29 @@ const printFizz = "FIZZ";
 
 const printBuzz = "BUZZ";
 
-const printFizzBuzz = printFizz + printBuzz
+const printFizzBuzz = printFizz + printBuzz;
+
+const ulElement = document.querySelector("ul");
 
 for (i = 1; i <= 100; i++){
+    const liElement = document.createElement("li");
+
     if (i % 3 === 0 && i % 5 === 0){
         console.log(printFizzBuzz);
+        liElement.append("FIZZBUZZ")
     } else if (i % 3 === 0){
         console.log(printFizz);
+
+        liElement.append("FIZZ");
     } else if (i % 5 === 0){
         console.log(printBuzz);
+
+        liElement.append("BUZZ");
     } else{
         console.log(i);
+        
+        liElement.append(i);
     };
+
+    ulElement.append(liElement)
 };
