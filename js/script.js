0490dc7c1b310557ac6e57c3d7aab66fb8de89d9
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-16 15:51:34 +0200

    fixed y spacing

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const printFizz = "FIZZ";
 const printBuzz = "BUZZ";
 
 const printFizzBuzz = printFizz + printBuzz
+
 for (i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0){
         console.log(printFizzBuzz);
