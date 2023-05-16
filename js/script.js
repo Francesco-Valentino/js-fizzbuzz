2507868dc3dd1393789054b0acd7c805eea17862
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-16 16:40:14 +0200

    completed exercise with both bonuses

--- js/script.js
+++ js/script.js
@@ -11,18 +11,29 @@ for (i = 1; i <= 100; i++){
 
     if (i % 3 === 0 && i % 5 === 0){
         console.log(printFizzBuzz);
-        liElement.append("FIZZBUZZ")
+
+        liElement.append("FIZZBUZZ");
+
+        liElement.classList.add("fizzBuzzText");
     } else if (i % 3 === 0){
         console.log(printFizz);
 
         liElement.append("FIZZ");
+
+        //liElement.classList.remove("fizzBuzzText");
+
+        liElement.classList.add("fizzText");
     } else if (i % 5 === 0){
         console.log(printBuzz);
 
         liElement.append("BUZZ");
+
+        //liElement.classList.remove("fizzText");
+
+        liElement.classList.add("buzzText");
     } else{
         console.log(i);
-        
+
         liElement.append(i);
     };
 
